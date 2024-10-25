c7f75aa7bd41187e1016a671e34b079d3fbf5fb3
GUSEUL <sbeenjung@gmail.com>
2024-10-25 09:53:09 +0900

    Update math_utils.py
    .

--- _utils/math_utils.py
+++ _utils/math_utils.py
@@ -10,6 +10,11 @@ __all__ = ["is_prime", "generate_prime", "mod_exp", "mod_inverse", "gcd", "discr
 def gauss_func(n: float) -> int:
     return int(n) 
 
+def is_generater(n: int) -> bool:
+    ret = True
+
+    return ret
+
 def is_prime_1(n: int) -> bool: # Trial Devision
     test_mod = gauss_func(n**0.5)
     ret = True
@@ -18,6 +23,19 @@ def is_prime_1(n: int) -> bool: # Trial Devision
             return False
         break
     return ret
+
+def is_prime_2(n: int) -> bool: # fermat's primarily test
+    for i in range(2, n+1):
+        if is_generater(i) == True:
+            return i
+        break
+
+    if (i**(n-1)) % n == 1:
+        return True
+    else:
+        return False
+
+
 
 
 
