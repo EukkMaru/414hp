e13b35348359006379f85a18087066c92f301581
GUSEUL <sbeenjung@gmail.com>
2024-10-25 09:35:45 +0900

    is_prime_1

--- _utils/math_utils.py
+++ _utils/math_utils.py
@@ -10,9 +10,16 @@ __all__ = ["is_prime", "generate_prime", "mod_exp", "mod_inverse", "gcd", "discr
 def gauss_func(n: float) -> int:
     return int(n) 
 
+def is_prime_1(n: int) -> bool: # Trial Devision
+    test_mod = gauss_func(n**0.5)
+    ret = True
+    for i in range(2, test_mod + 1):
+        if n % i == 0:
+            return False
+        break
+    return ret
 
-def is_prime(n: int) -> bool:
-    pass
+
 
 def generate_prime(bits: int=2) -> int:
     pass
