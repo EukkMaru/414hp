ab87fcde3b44423e24bf9f7bea9cda82cc49bce0
GUSEUL <sbeenjung@gmail.com>
2024-10-25 09:02:23 +0900

    kkk

---
+++ _utils/math_utils.py
@@ -0,0 +1,27 @@
+# import 
+from typing import *
+
+__all__ = ["is_prime", "generate_prime", "mod_exp", "mod_inverse", "gcd", "discrete_log"]
+
+# def _new_func():
+
+# def is_prime(n, size): 
+def is_prime(n: int) -> bool:
+    pass
+
+def generate_prime(bits: int=2) -> int:
+    pass
+
+def mod_inverse(a, m):
+    pass
+
+def gcd(a, b):
+    pass
+
+def discrete_log(g, h, p):
+    pass
+
+if __name__ == "__main__":
+    # Testing code goes here
+    # print(is_prime(32), is_prime(31))...
+    pass
